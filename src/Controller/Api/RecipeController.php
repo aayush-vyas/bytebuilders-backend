6d2666830164bde977f41a1750c5d129e2cfc56c
mihir-someshwara <msomeshwara@dastechnology.com>
2024-07-21 02:57:57 +0530

    feature: added yt links in recipe response object

--- src/Controller/Api/RecipeController.php
+++ src/Controller/Api/RecipeController.php
@@ -3,8 +3,10 @@
 // src/Controller/Api/AllRecipeController.php
 namespace App\Controller\Api;
 
+use App\Entity\Recipe;
 use App\Services\RecipeService;
 use App\Services\SpoonacularApiService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RecipeController extends AbstractController
 {
-    public function __construct(private SpoonacularApiService $spoonacularApiService, private RecipeService $recipeService)
+    public function __construct(private SpoonacularApiService $spoonacularApiService, private RecipeService $recipeService, private EntityManagerInterface $em)
     {
     }
 
